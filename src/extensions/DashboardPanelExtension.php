5b098e6bb22ac197ae5fc0bf1461f594139b7ae2
Morven Lewis-Everley <morven@ilateral.co.uk>
2020-06-05 11:06:49 +0100

    Remove uneeded panels

--- src/extensions/DashboardPanelExtension.php
+++ src/extensions/DashboardPanelExtension.php
@@ -5,10 +5,8 @@ namespace SilverCommerce\Dashboard\Extension;
 use SilverStripe\ORM\DB;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\SiteConfig\SiteConfig;
-use SilverCommerce\Dashboard\Panel\LowStockPanel;
 use SilverCommerce\Dashboard\Panel\TopProductsPanel;
-use SilverCommerce\Dashboard\Panel\NewCustomersPanel;
-use SilverCommerce\Dashboard\Panel\ContentSummaryPanel;
+use SilverCommerce\Dashboard\Panel\OrderStatusPanel;
 use SilverCommerce\Dashboard\Panel\RecentOrdersListPanel;
 use SilverCommerce\Dashboard\Panel\RecentOrdersChartPanel;
 
@@ -30,7 +28,7 @@ class DashboardPanelExtension extends DataExtension
             $panel->write();
 
             // Add content summary panel
-            $panel = ContentSummaryPanel::create();
+            $panel = OrderStatusPanel::create();
             $panel->Title = $panel->getLabel();
             $panel->SiteConfigID = $config->ID;
             $panel->SortOrder = 2;
@@ -43,13 +41,6 @@ class DashboardPanelExtension extends DataExtension
             $panel->SortOrder = 3;
             $panel->write();
 
-            // Add low stock items panel
-            $panel = LowStockPanel::create();
-            $panel->Title = $panel->getLabel();
-            $panel->SiteConfigID = $config->ID;
-            $panel->SortOrder = 4;
-            $panel->write();
-
             // Add top products panel
             $panel = TopProductsPanel::create();
             $panel->Title = $panel->getLabel();
@@ -57,13 +48,6 @@ class DashboardPanelExtension extends DataExtension
             $panel->SortOrder = 5;
             $panel->write();
 
-            // Add new customer panel
-            $panel = NewCustomersPanel::create();
-            $panel->Title = $panel->getLabel();
-            $panel->SiteConfigID = $config->ID;
-            $panel->SortOrder = 6;
-            $panel->write();
-            
             DB::alteration_message('Created default commerce dashboard', 'created');
         }
     }
